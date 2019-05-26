eec807c5f3b933ed780a3ec8e10e5ed8e2b384aa
JIHONGKIM <john_over2@naver.com>
2019-05-27 00:14:54 +0900

    지니 차트 크롤링

---
+++ Python_crawler/parser1.py
@@ -0,0 +1,12 @@
+import urllib.request
+import requests
+from bs4 import BeautifulSoup
+def get_html(url):
+   _html = ""
+   resp = requests.get(url)
+   if resp.status_code == 200:
+       _html = resp.text
+   return _html
+html = get_html("https://genie.co.kr/chart/top200")
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.find_all("a",{"class":"title ellipsis"})[1])
