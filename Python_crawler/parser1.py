fd733b1e936bc4d9f6a919d9cfecb9e3d61ff406
jihongeek <hong010910@daum.net>
2019-06-06 01:37:43 +0900

    hi

--- Python_crawler/parser1.py
+++ Python_crawler/parser1.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 def get_html(url):
    _html = ""
    resp = requests.get(url)
@@ -8,4 +9,4 @@ def get_html(url):
    return _html
 html = get_html("https://genie.co.kr/chart/top200")
 soup = BeautifulSoup(html, 'html.parser')
-print(soup.find_all("a",{"class":"title ellipsis"})[1])
+print(soup.find_all("a", {"class": "title ellipsis"}))
