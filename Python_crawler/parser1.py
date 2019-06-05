5c29cc2c3ae77541fa8017056ef6acbf233d612a
jihongeek <hong010910@daum.net>
2019-06-06 01:39:57 +0900

    수정

--- Python_crawler/parser1.py
+++ Python_crawler/parser1.py
@@ -11,6 +11,3 @@ html = get_html("https://genie.co.kr/chart/top200")
 soup = BeautifulSoup(html, 'html.parser')
 <<<<<<< HEAD
 print(soup.find_all("a", {"class": "title ellipsis"}))
-=======
-print(soup.find_all("a",{"class":"title ellipsis"}))
->>>>>>> 68f65da1db1c8d4cac42017a7164ff90429183b5
